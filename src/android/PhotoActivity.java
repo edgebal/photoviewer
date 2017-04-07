44673615fcab87e2ef0ca9b7fadda835104b58c9
Emiliano Balbuena <edgebal@gmail.com>
2017-04-07 15:46:46 -0300

    Initial Glide implementation

--- src/android/PhotoActivity.java
+++ src/android/PhotoActivity.java
@@ -18,7 +18,11 @@ import android.net.Uri;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.squareup.picasso.Picasso;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -131,28 +135,31 @@ public class PhotoActivity extends Activity {
 	}
 
 	/**
-	 * Load the image using Picasso
+	 * Load the image using Glide
 	 *
 	 */
 	private void loadImage() {
-		if( imageUrl.startsWith("http") || imageUrl.startsWith("file") ) {
-		Picasso.with(this)
+		if (imageUrl.startsWith("http")) {
+			Glide.with(this)
 				.load(imageUrl)
-				.fit()
-				.centerInside()
-				.into(photo, new com.squareup.picasso.Callback() {
+				.diskCacheStrategy(DiskCacheStrategy.SOURCE)
+				.fitCenter()
+				.listener(new RequestListener<String, GlideDrawable>() {
 					@Override
-					public void onSuccess() {
+					public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
 						hideLoadingAndUpdate();
+						return false;
 					}
 
 					@Override
-					public void onError() {
+					public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
 						Toast.makeText(getActivity(), "Error loading image.", Toast.LENGTH_LONG).show();
 
 						finish();
+						return false;
 					}
-				});
+				})
+				.into(photo);
 	} else if ( imageUrl.startsWith("data:image")){
             String base64String = imageUrl.substring(imageUrl.indexOf(",")+1);
             byte[] decodedString = Base64.decode(base64String, Base64.DEFAULT);
